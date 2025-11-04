04fff305631181022e2a1063e7300fefcfd20974
Petros <93052465+pkitazos@users.noreply.github.com>
2025-11-04 20:45:34 +0100

    finished mvp actor runtime - unbounded mailbox (no backpressure, susceptible to OOM) - actors can't handle unknown messages (blow up, though you could just have them ignore or flush those out) - no supervision - cannot turn one actor into another arbitrarily behaving actor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::ops::Add;
-
 use tokio::sync::mpsc::{self, UnboundedSender};
 
 struct Addr<M> {
@@ -15,8 +13,8 @@ impl<M> Clone for Addr<M> {
 }
 
 impl<M> Addr<M> {
-    fn send(&self, envelope: M) {
-        let _ = self.mb.send(envelope);
+    fn send(&self, msg: M) {
+        let _ = self.mb.send(msg);
     }
 }
 
@@ -56,4 +54,90 @@ where
 
 // ---
 
-fn main() {}
+enum Pinger {
+    Idle,
+    Murderous,
+    Dead,
+}
+
+impl Pinger {
+    fn send_to(to: Addr<PingMsg>, msg: PingMsg) {
+        to.send(msg);
+    }
+}
+
+enum PingMsg {
+    Ping { reply_to: Addr<PongMsg> },
+    DeadlyPing,
+}
+
+impl Behaviour for Pinger {
+    type Msg = PongMsg;
+    fn apply(&mut self, msg: Self::Msg, self_addr: &Addr<Self::Msg>) -> Effect {
+        let PongMsg::Pong { reply_to } = msg;
+
+        match self {
+            Pinger::Idle => {
+                println!("[Pinger] Got pong. If bro says one more word..");
+                reply_to.send(PingMsg::Ping {
+                    reply_to: self_addr.clone(),
+                });
+                *self = Pinger::Murderous;
+                Effect::Continue
+            }
+            Pinger::Murderous => {
+                reply_to.send(PingMsg::DeadlyPing);
+                println!("[Pinger] Got pong. I must now end this mf");
+                *self = Pinger::Dead; // dies from guilt
+                Effect::Continue
+            }
+            Pinger::Dead => Effect::Die,
+        }
+    }
+}
+
+enum Ponger {
+    Idle,
+    Dead,
+}
+
+impl Ponger {
+    fn send_to(to: Addr<PongMsg>, msg: PongMsg) {
+        to.send(msg);
+    }
+}
+enum PongMsg {
+    Pong { reply_to: Addr<PingMsg> },
+}
+
+impl Behaviour for Ponger {
+    type Msg = PingMsg;
+    fn apply(&mut self, msg: Self::Msg, self_addr: &Addr<Self::Msg>) -> Effect {
+        match (&*self, msg) {
+            (Ponger::Idle, PingMsg::Ping { reply_to }) => {
+                println!("[Ponger] Got ping!");
+                reply_to.send(PongMsg::Pong {
+                    reply_to: self_addr.clone(),
+                });
+                *self = Ponger::Idle;
+                Effect::Continue
+            }
+            (Ponger::Idle, PingMsg::DeadlyPing) => {
+                println!("[Ponger] Got pint! Man, this ping looks funny...");
+                *self = Ponger::Dead;
+                Effect::Die
+            }
+            _ => Effect::Die, // already dead or unexpected message
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let pinger = spawn_actor(Pinger::Idle);
+    let ponger = spawn_actor(Ponger::Idle);
+
+    Pinger::send_to(ponger, PingMsg::Ping { reply_to: pinger });
+
+    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+}
