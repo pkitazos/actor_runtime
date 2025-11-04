d71d79f525a847335a80ec53c77a29cd548930ad
Petros <93052465+pkitazos@users.noreply.github.com>
2025-11-04 20:09:14 +0100

    simplified actor representation effects can be modelled as enums state can be mutable and so when an actor with a given behaviour receives a message it can mutate its internal state and return whatever effect is appropriate

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,52 @@
-use std::{any::Any, sync::Arc};
+use std::ops::Add;
+
 use tokio::sync::mpsc::{self, UnboundedSender};
 
-struct Env;
-
-enum Next<B> {
-    Stay(B),
-    Become(B),
-    Stop,
-}
-
-#[derive(Clone)]
 struct Addr<M> {
-    mb: UnboundedSender<Envelope<M>>,
+    mb: UnboundedSender<M>,
 }
 
-impl<M> Addr<M> {
-    fn send(&self, envelope: Envelope<M>) {
-        let _ = self.mb.send(envelope);
-    }
-}
-
-#[derive(Clone)]
-struct ErasedAddr {
-    // unless we know ahead of time exactly what actors we are going to be communicating with,
-    // we would not be able to properly type the address that will be included with every message we receive
-    // but we know that an address is something that can be sent a message of some shape
-    // so we can represent it as a struct with a send field which is a function pointer that sends some thing
-    //
-    // We can’t put a statically typed Addr<T> inside every envelope because receivers don’t know the sender’s T.
-    // So we store an erased address: a trait object wrapping a closure that knows how to deliver a boxed envelope
-    // to the original sender if the type matches.
-    send: Arc<dyn Fn(Box<dyn Any + Send>) + Send + Sync>,
-}
-
-impl<M: 'static + Send> From<&Addr<M>> for ErasedAddr {
-    fn from(a: &Addr<M>) -> ErasedAddr {
-        let a = a.clone();
-        ErasedAddr {
-            send: Arc::new(move |boxed| {
-                if let Ok(envelope) = boxed.downcast::<Envelope<M>>() {
-                    let _ = a.send(*envelope);
-                } else {
-                    // tried to send a message this actor cannot receive
-                    // log it or something
-                }
-            }),
+impl<M> Clone for Addr<M> {
+    fn clone(&self) -> Self {
+        Addr {
+            mb: self.mb.clone(),
         }
     }
 }
 
-struct Envelope<M> {
-    msg: M,
-    addr: ErasedAddr,
+impl<M> Addr<M> {
+    fn send(&self, envelope: M) {
+        let _ = self.mb.send(envelope);
+    }
 }
 
-trait Behaviour {
+// a behaviour just takes a message and returns some effect
+trait Behaviour: Send + 'static {
     type Msg: Send + 'static;
 
-    fn self_addr(self) -> Addr<Self::Msg>;
+    fn apply(&mut self, msg: Self::Msg, self_addr: &Addr<Self::Msg>) -> Effect;
+}
 
-    fn on(self, envelope: Envelope<Self::Msg>, env: &mut Env) -> Next<Self>
-    where
-        Self: Sized;
+enum Effect {
+    Continue, // either continue doing something
+    Die,      // or die
 }
 
 fn spawn_actor<B>(initial: B) -> Addr<B::Msg>
 where
     B: Behaviour + Send + 'static,
 {
-    let (tx, mut rx) = mpsc::unbounded_channel::<Envelope<B::Msg>>();
+    let (tx, mut rx) = mpsc::unbounded_channel::<B::Msg>();
     let addr = Addr { mb: tx };
+    let addr_copy = addr.clone();
 
     tokio::spawn(async move {
         let mut state = initial;
-        let mut env = Env;
 
-        while let Some(envelope) = rx.recv().await {
-            state = match state.on(envelope, &mut env) {
-                Next::Stay(s) | Next::Become(s) => s,
-                Next::Stop => break,
+        while let Some(msg) = rx.recv().await {
+            match state.apply(msg, &addr_copy) {
+                Effect::Continue => continue,
+                Effect::Die => break,
             };
         }
     });
